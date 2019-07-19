b90381e6c9d7e580757e342dfe31021bfaa9a18d
Gaurav <gameider12@gmail.com>
2019-07-18 22:48:13 -0400

    plot3 added plot2 modified

--- plot2.R
+++ plot2.R
@@ -3,8 +3,8 @@ dates <- dat$Date
 dates <- strptime(dates,'%d/%m/%Y')
 dt2007 <- which(dates$year == 107 & dates$mon == 1 & (dates$mday == 1 | dates$mday == 2)) #dates since 1900 monts 011
 df <- dat[dt2007,]
-df$dt <- paste(df$Date,df$Time)
 df$Date <- as.Date(df$Date,"%d/%m/%Y")
+df$dt <- paste(df$Date,df$Time)
 df$dt <- strptime(df$dt,"%Y-%m-%d %H:%M:%S")
 png(filename = "plot2.png",height = 480, width = 480)
 plot(df$dt,df$Global_active_power,type = 'l',xlab = "",ylab = "Global Active Power(kilowatts)")
